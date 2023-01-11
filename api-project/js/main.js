61ab35438520f73d44c21fa8fcffa2eb020d1778
tiffyy101 <bobatiffy101@gmail.com>
2023-01-11 10:21:56 -0500

    2

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,9 +1,15 @@
 import '/styles/style.css'
 
-const URL = "https://fruityvice.com/api/fruit/all";
+const URL = "https://www.themuse.com/api/public/companies";
 
 async function getData(URL){
-  const response = await fetch(URL);
-  console.log(response);
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data.content);
+    document.getElementById("response").textContent = data.content;
+  }catch (error) {
+    console.log(error);
+  }
 }
 getData(URL);
