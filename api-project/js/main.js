6aded525fc35cd186173cc8e2b8acce148004a76
tiffyy101 <bobatiffy101@gmail.com>
2023-01-13 10:07:09 -0500

    4

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,13 +1,21 @@
 import '/styles/style.css'
 
-const URL = "https://www.themealdb.com/api/json/v1/1/search.php?f=c";
+const URL = "https://cat-fact.herokuapp.com/facts";
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data.meals.strInstructions);
-    document.getElementById("response").textContent = data.meals.strInstructions;
+    console.log(data.text);
+
+    function showText() {
+      data.data.forEach((text) => document.getElementById("response").insertAdjacentHTML(
+        "beforeend",
+            `
+            <h2>${text.text}`
+      ))
+    }
+    document.getElementById("response").textContent = data.text;
   } catch (error) {
     console.log(error);
   }
