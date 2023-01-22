257a97010e2c219cb806511d28172669fbbe732c
tiffyy101 <bobatiffy101@gmail.com>
2023-01-22 18:18:59 -0500

    idk

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,29 +1,42 @@
 import '/styles/style.css'
 
-const URL = "https://www.themealdb.com/api/json/v1/1/random.php";
+const DOMSelectors = {
+  search: document.getElementById("search"),
+  input: document.getElementById("input"),
+  btn: document.getElementById("btn"),
+  result: document.getElementById("result")
+};
+
+const URL = "https://api.open5e.com/monsters/?search";
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data.meals.strMeal);
-    document.getElementById("response").textContent = data.meals.strMeal;
-  } catch (error) {
+    console.log(data);
+    data.data.forEach((a) => {
+      console.log(a.results.name, a.results.alignment, a.results.skills);
+      DOMSelectors.result.insertAdjacentHTML(
+        "beforeend",
+        `<div class="character">
+        <h2>${a.results.name}</h2>
+        <h3>${a.results.alignment}</h3>
+        <p>${a.results.skills}</p>
+        </div>
+        `
+      );
+    });
+  }catch (error) {
     console.log(error);
   }
-
-/*     function showFood() {
-      data.meals.forEach((food) => document.getElementById("response").insertAdjacentHTML(
-        "afterbegin",
-            `
-            <h2>cook ${food.meals.strMeal}</h2>
-            <p>${food.meals.strIntrusctions}</p>`
-      ))
-    }
-
-data.meals
-.forEach((food) => showFood(food)); */
-
-
 }
 getData(URL);
+
+async function searchAPI() {
+  try {
+    DOMSelectors.btn.addEventListener("submit", function clearInput(){
+      DOMSelectors.content = "";
+    });
+  }catch (error) {}
+}
+searchAPI();
