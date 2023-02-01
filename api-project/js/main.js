254868f7e46710d315aab096fc31cde888580bb7
tiffyy101 <bobatiffy101@gmail.com>
2023-01-31 19:45:29 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -6,112 +6,115 @@ const DOMSelectors = {
   input: document.getElementById("input"),
   btn: document.getElementById("btn"),
   display: document.getElementById("display"),
-  allCharacters: document.getElementById("allCharacters"),
-  chaoticEvil: document.getElementById("chaoticEvil"),
-  neutral: document.getElementById("neutral"),
-  lawfulGood: document.getElementById("lawfulGood"),
-  mainDiv: document.getElementById("main")
+  sword: document.getElementById("sword"),
+  claymore: document.getElementById("claymore"),
+  bow: document.getElementById("bow"),
+  all: document.getElementById("all")
+
 };
 
-const URL = "https://api.open5e.com/monsters";
+const characters = "https://genshinlist.com/api/characters";
 
-async function getData(URL) {
+async function getData(characters) {
   try {
-    const response = await fetch(URL);
+    const response = await fetch(characters);
     if (response.status < 200 || response.status > 299) {
-      throw new error(response);
+      throw new Error(response);
     } else {
-    const data = await response.json();
+      const data = await response.json();
+ 
 
-    function allCharacters(results) {
-      results.data
-      .forEach((character) => {
-        DOMSelectors.display.innerHTML = " ";
-        DOMSelectors.mainDiv.insertAdjacentHTML
-        ("beforeend",
-                `
-              <h2>${character.results.name}</h2>
-              <h3>${character.results.alignment}`)
-    });
-  }
-    
-/*   const chaoticEvil = document.getElementById("chaoticEvil");
-  chaoticEvil.addEventListener("click", function () {
-      DOMSelectors.display.innerHTML = " ";
-      menu.filter((chaoticEvil) => chaoticEvil.results.alignment === "yes")
-      .forEach(bestSeller => {
-          DOMSelectors.mainDiv.insertAdjacentHTML */
+      function all() {
+        data.forEach((data) => {
+          DOMSelectors.display.insertAdjacentHTML
+            ("beforeend",
+              `
+              <h2>${data.name}</h2>
+              <h3>Gender:${data.gender}</h3>
+              <p>${data.description}</p>
+              <button id="choose" class="choose">Choose</button>
+          </div>
+          <div id="mainCard">`
+            )
+        });
+      }
 
-    function chaoticEvil(results) {
-      DOMSelectors.display.innerHTML = "";
-      results.data
-      .filter((character) => character.results.alignment === "chaotic evil")
-      .forEach((character) => {
-        DOMSelectors.mainDiv.insertAdjacentHTML
-        ("beforeend",
+      function sword() {
+        data.filter((results) => results.weapon === "sword")
+          .forEach(results => {
+            DOMSelectors.display.insertAdjacentHTML              
+            ("beforeend",
                 `
-              <h2>${character.results.name}</h2>
-              <h3>${character.results.alignment}`)
+                <h2>${results.name}</h2>
+                <h3>Gender:${results.gender}</h3>
+                <p>${results.description}</p>
+                <button id="choose" class="choose">Choose</button>
+          </div>
+          <div id="mainCard">`)
+          });
+      }
+
+      function claymore() {
+        data.filter((results) => results.weapon === "claymore")
+          .forEach(results => {
+            DOMSelectors.display.insertAdjacentHTML              
+            ("beforeend",
+                `
+                <h2>${results.name}</h2>
+                <h3>Gender:${results.gender}</h3>
+                <p>${results.description}</p>
+                <button id="choose" class="choose">Choose</button>
+          </div>
+          <div id="mainCard">`)
+          });
+      }
+
+      function bow() {
+        data.filter((results) => results.weapon === "bow")
+          .forEach(results => {
+            DOMSelectors.display.insertAdjacentHTML              
+            ("beforeend",
+                `
+                <h2>${results.name}</h2>
+                <h3>Gender:${results.gender}</h3>
+                <p>${results.description}</p>
+                <button id="choose" class="choose">Choose</button>
+          </div>
+          <div id="mainCard">`)
+          });
+      }
+
+      DOMSelectors.all.addEventListener("click", function () {
+        all();
+        });
+
+    DOMSelectors.sword.addEventListener("click", function () {
+      sword();
       });
+   
+    DOMSelectors.claymore.addEventListener("click", function () {
+      claymore();
+    });
+   
+    DOMSelectors.bow.addEventListener("click", function () {
+      bow();
+    });
+
+     console.log(data);
     }  
-    
-    function neutral(results) {
-      DOMSelectors.display.innerHTML = "";
-      results.data
-      .filter((character) => character.results.alignment === "neutral")
-      .forEach((character) => {
-        DOMSelectors.mainDiv.insertAdjacentHTML
-        ("beforeend",
-                `
-              <h2>${character.results.name}</h2>
-              <h3>${character.results.alignment}`)
-      });
-    }
-
-    function lawfulGood(results) {
-      DOMSelectors.display.innerHTML = "";
-      results.data
-      .filter((character) => character.results.alignment === "lawful good")
-      .forEach((character) => {
-        DOMSelectors.mainDiv.insertAdjacentHTML
-        ("beforeend",
-                `
-              <h2>${character.results.name}</h2>
-              <h3>${character.results.alignment}`)
-      });
-    }
-
-    DOMSelectors.allCharacters.addEventListener("click", function () {
-      allCharacters(data);
-    });
-
-    DOMSelectors.chaoticEvil.addEventListener("click", function () {
-      chaoticEvil(data);
-    });
-
-    DOMSelectors.neutral.addEventListener("click", function () {
-      neutral(data);
-    });
-
-    DOMSelectors.lawfulGood.addEventListener("click", function () {
-      lawfulGood(data);
-    });
-
-    console.log(data);
-  }
   } catch (error) {
     console.log(error);
     console.log("can't find");
   }
 }
-getData(URL);
+getData(characters);
 
 document.querySelector(".btnTheme").addEventListener("click", function () {
-  if(document.body.classList.contains("warm")) {
-      document.body.classList.add("cool");
-      document.body.classList.remove("warm");
+  if (document.body.classList.contains("warm")) {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
   } else {
-      document.body.classList.add("warm");
-      document.body.classList.remove("cool");
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
   }
 });
